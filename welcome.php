f7e54d815b58886a1a0223106735900f18d51fd2
Michael Vuong <mvuo803@cse.unsw.edu.au>
2014-04-08 11:31:12 +1000

    changed home page to welcome page, bootstrap css is working

---
+++ welcome.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title>Welcome</title>
+		<!-- bootstrap css file -->
+		<link href="style/bootstrap-3.1.1-dist/css/bootstrap.min.css" rel="stylesheet">
+		
+	</head>
+	
+	<body>
+		<div class="jumbotron" align="center">
+			<h1> Welcome </h1>
+			<p> Upload Download Rate Comment </p>
+			<p><a href="#" class="btn btn-primary btn-lg" role="button"> Log In &raquo;</a></p>
+		</div>
+			
+			
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
